43bc1eafad04adb73965ffbf57bb04cb8de6118d
Simon-Bertram <simonbertram@hotmail.com>
2024-08-13 09:15:27 +0100

    add Tailwind config for fonts and containers

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,45 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            fontFamily: theme("fontFamily.sans").join(","),
+            // other typography settings...
+          },
+        },
+      }),
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
